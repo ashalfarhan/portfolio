7e456f1cb0b26aa041b3f23c89ae43931543e7a3
ashalfarhan <ashalfarhan8@gmail.com>
2022-05-26 17:47:41 +0700

    init lighthouse

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,12 @@
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
-import Script from 'next/script';
+// import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      themes={['light', 'dark']}
-      attribute="class"
-      defaultTheme="dark"
-    >
-      <Script
+    <ThemeProvider attribute="class" defaultTheme="system">
+      {/* <Script
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-0TZP917KRV"
       />
@@ -19,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-0TZP917KRV');`}
-      </Script>
+      </Script> */}
       <Component {...pageProps} />
     </ThemeProvider>
   );
